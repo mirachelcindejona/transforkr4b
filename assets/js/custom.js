4263ba47a3093ce306a3a2be747c4acc9391960a
mirachelcindejona <mirachelcj@gmail.com>
2023-12-25 20:45:30 +0700

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,159 @@
+(function ($) {
+  "use strict";
+
+  // Window Resize Mobile Menu Fix
+  mobileNav();
+
+  // Scroll animation init
+  window.sr = new scrollReveal();
+
+  // Menu Dropdown Toggle
+  if ($(".menu-trigger").length) {
+    $(".menu-trigger").on("click", function () {
+      $(this).toggleClass("active");
+      $(".header-area .nav").slideToggle(200);
+    });
+  }
+
+  // Menu elevator animation
+  $("a[href*=\\#]:not([href=\\#])").on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        var width = $(window).width();
+        if (width < 991) {
+          $(".menu-trigger").removeClass("active");
+          $(".header-area .nav").slideUp(200);
+        }
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 130,
+          },
+          700
+        );
+        return false;
+      }
+    }
+  });
+
+  $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on("click", function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $("a").each(function () {
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+
+      var target = this.hash,
+        menu = target;
+      var target = $(this.hash);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: target.offset().top - 130,
+          },
+          500,
+          "swing",
+          function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+          }
+        );
+    });
+  });
+
+  // Home seperator
+  if ($(".home-seperator").length) {
+    $(".home-seperator .left-item, .home-seperator .right-item").imgfix();
+  }
+
+  // Home number counterup
+  if ($(".count-item").length) {
+    $(".count-item strong").counterUp({
+      delay: 10,
+      time: 1000,
+    });
+  }
+
+  // Page loading animation
+  $(window).on("load", function () {
+    if ($(".cover").length) {
+      $(".cover").parallax({
+        imageSrc: $(".cover").data("image"),
+        zIndex: "1",
+      });
+    }
+
+    $("#preloader").animate(
+      {
+        opacity: "0",
+      },
+      600,
+      function () {
+        setTimeout(function () {
+          $("#preloader").css("visibility", "hidden").fadeOut();
+        }, 300);
+      }
+    );
+  });
+
+  // Window Resize Mobile Menu Fix
+  $(window).on("resize", function () {
+    mobileNav();
+  });
+
+  // Window Resize Mobile Menu Fix
+  function mobileNav() {
+    var width = $(window).width();
+    $(".submenu").on("click", function () {
+      if (width < 992) {
+        $(".submenu ul").removeClass("active");
+        $(this).find("ul").toggleClass("active");
+      }
+    });
+  }
+
+  //Scroll back to top
+
+  var progressPath = document.querySelector(".progress-wrap path");
+  var pathLength = progressPath.getTotalLength();
+  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+  progressPath.style.strokeDashoffset = pathLength;
+  progressPath.getBoundingClientRect();
+  progressPath.style.transition = progressPath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
+  var updateProgress = function () {
+    var scroll = $(window).scrollTop();
+    var height = $(document).height() - $(window).height();
+    var progress = pathLength - (scroll * pathLength) / height;
+    progressPath.style.strokeDashoffset = progress;
+  };
+  updateProgress();
+  $(window).scroll(updateProgress);
+  var offset = 50;
+  var duration = 550;
+  jQuery(window).on("scroll", function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery(".progress-wrap").addClass("active-progress");
+    } else {
+      jQuery(".progress-wrap").removeClass("active-progress");
+    }
+  });
+  jQuery(".progress-wrap").on("click", function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, duration);
+    return false;
+  });
+})(window.jQuery);
